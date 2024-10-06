ad017e2acda396948ee3bd8b99722c34c62c4c3b
saurabh14 <gsaurabh1407@gmail.com>
2024-10-06 16:54:34 +0530

    6th Oct

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  const TitleNew = () => <h1 className='saurabh'>testing JSX</h1>
+  const saurabh = <h1>saurabh element</h1>
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +13,7 @@ function App() {
           Saurabh is testing
           <div>Testingnpm</div>
           babli
+          {saurabh}
         </p>
         <a
           className="App-link"
@@ -19,9 +23,12 @@ function App() {
         >
           Learn React
         </a>
+        <TitleNew />
       </header>
     </div>
+    
   );
+
 }
 
 export default App;
